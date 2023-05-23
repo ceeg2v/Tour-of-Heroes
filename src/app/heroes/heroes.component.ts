60527b7b49ad9c5eb7a37b01b78f5fc7b572c303
ceeg2v <111925520+ceeg2v@users.noreply.github.com>
2023-05-23 12:20:48 -0500

    Initial commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent {
+  selectedHero?: Hero;
+
+  heroes : Hero[] = [];
+
+  constructor(private heroService: HeroService) {}
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  
+}
+
+
