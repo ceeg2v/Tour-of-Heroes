a7d04f41163a4586efd92e64ca4b1d9c73d5e875
ceeg2v <111925520+ceeg2v@users.noreply.github.com>
2023-05-25 15:10:22 -0500

    Stage 5

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
 
   heroes : Hero[] = [];
@@ -15,16 +16,13 @@ export class HeroesComponent {
   constructor(private heroService: HeroService) {}
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
   ngOnInit(): void {
     this.getHeroes();
   }
   
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
   
 }
 
